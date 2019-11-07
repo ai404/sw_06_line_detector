7ba20cb8668af0182293c8dfc00f21f967e8875a
ai404 <khalifa.abde@outlook.com>
2019-11-06 21:08:51 -0500

    another commit

--- packages/sw_06_line_detector/include/sw_06_line_detector/sw_06_cv_functions.py
+++ packages/sw_06_line_detector/include/sw_06_line_detector/sw_06_cv_functions.py
@@ -9,23 +9,23 @@ import rospy
 ## CATEGORY 1
 def inRange(hsv_image, low_range, high_range):
 	# reshape thershold vectors
-	low_range = low_range.reshape(1,1,-1)
-	high_range = high_range.reshape(1,1,-1)
+	#low_range = low_range.reshape(1,1,-1)
+	#high_range = high_range.reshape(1,1,-1)
 
 	# >= because of inclusive boundaries
-	return np.prod((hsv_image>=low_range)&(hsv_image<=high_range),-1)*255
-	#return cv2.inRange(hsv_image, low_range, high_range)
+	#return np.prod((hsv_image>=low_range)&(hsv_image<=high_range),-1)*255
+	return cv2.inRange(hsv_image, low_range, high_range)
 
 def bitwise_or(bitwise1, bitwise2):
-	return bitwise1 | bitwise2
-	#return cv2.bitwise_or(bitwise1, bitwise2)
+	#return bitwise1 | bitwise2
+	return cv2.bitwise_or(bitwise1, bitwise2)
 
 def bitwise_and(bitwise1, bitwise2):
-	return bitwise1 & bitwise2
-	#return cv2.bitwise_and(bitwise1, bitwise2)
+	#return bitwise1 & bitwise2
+	return cv2.bitwise_and(bitwise1, bitwise2)
 
 def getStructuringElement(shape, size):
-
+    """
     MORPH_RECT = 0
     MORPH_CROSS = 1
     MORPH_ELLIPSE = 2
@@ -58,23 +58,26 @@ def getStructuringElement(shape, size):
             np_obj[j1:j2,i] = 1
 
     return np_obj.T
-	#return cv2.getStructuringElement(shape, size)
+    """
+	return cv2.getStructuringElement(shape, size)
 
 def dilate(bitwise, kernel):
+    """
     w,h = kernel.shape
     pad_w = w//2
     pad_h = h//2
 	# pad the bitwise matrix with zeros
     pad_bitwise  = np.pad(bitwise, [(pad_w, pad_w), (pad_h, pad_h)], mode='constant', constant_values=0)
     def process(i, j):
-        """
+        "" "
 			git the matrix with the same size as the kernel, centered at i,j coordinates in bitwise
-        """
+        "" "
         shrunk = pad_bitwise[ i:i + w , j:j +h]
         return np.any(kernel & shrunk)
     return np.array([[ process(i,j) for j in range(bitwise.shape[1])] for i in range(bitwise.shape[0])],dtype=np.uint8)
 	#return binary_dilation(bitwise, kernel)
-	#return cv2.dilate(bitwise, kernel)
+    """
+	return cv2.dilate(bitwise, kernel)
 
 
 
